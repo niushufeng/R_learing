f11fa7817ce4edb757a025db380616fd89a6c838
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-15 15:55:57 +0800

    Update 27页.R

--- 2020_09_15/27页.R
+++ 2020_09_15/27页.R
@@ -19,3 +19,10 @@ quantile(mtcars$mpg) # 计算样本常用的分位数
 summary(mtcars$mpg)  # 计算样本的常用描述统计量
 length(mtcars$mpg)  # 返回向量的长度
  
+
+sum(mtcars$mpg) # 给出向量的总和
+prod(mtcars$mpg) # 给出向量的乘积
+rev(mtcars$mpg)  # 取向量的逆序
+sort(mtcars$mpg)  # 将向量按升序排列
+rank(mtcars$mpg)  # 返回向量的秩
+cumsum(mtcars$mpg) # 返回向量和向量的累计积
