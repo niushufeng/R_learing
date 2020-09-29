ee2ac70f8a53fedda395e34318461650a948f85b
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-29 15:45:18 +0800

    Update 课堂练习.R

--- 2020_09_29/课堂练习.R
+++ 2020_09_29/课堂练习.R
@@ -19,7 +19,7 @@ t(m1) %*% m1
 
 # 矩阵的行列式
 m2 <- matrix(1:9,3)
-det(m2) # 
+det(m2) # 求行列式的值
 ?det()
 
 
@@ -28,19 +28,26 @@ x <- c(42,7,64,9)
 y <- 1:4
 z.df <- data.frame(index = y , value = x)
 z.df
-rownames(z.df) <- c("row1","row2","row3","row4")
-colnames(z.df) <- c("col1","col2")
+#
+
+rownames(z.df) <- c("row1","row2","row3","row4")  # 更改行名
+colnames(z.df) <- c("col1","col2")  # 更改列名
 z.df[1,2] <- "apple" # 换值
 z.df
 
 z.df[1,2] <- 2
-z.df$col3 <- z.df$col1 + 3 
-row5 <- c(1,2,3)
+z.df$col3 <- z.df$col1 + 3  # 增加一列，值为第一列加3
 
+row5 <- c(1,2,3)  
 rbind(z.df,row5) # 增加行
-dim(z.df)
+
+dim(z.df) # 矩阵的维数 4行3列
 row.names(z.df[5,]) <- "row5"
 rownames(z.df[5,]) <- "row5_1"
+
+
+
+
 
 
 
