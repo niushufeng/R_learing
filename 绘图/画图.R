dba7909da38da25d81a2a2b7b80807bc2a970f2c
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-13 15:17:04 +0800

    Create 画图.R

---
+++ 绘图/画图.R
@@ -0,0 +1,23 @@
+# subset puromycin , subset
+attach(Puromycin)
+head(Puromycin)
+PuroA <- Puromycin[Puromycin$state == "treated",]
+PuroB <- Puromycin[Puromycin$conc > 0.5]
+
+# plot function,abline()
+plot(rate ~ conc,data = PuroA)
+abline(lm(rate ~cinc,data = PuroA))
+abline(a = 100,b =105,col = "blue")
+abline(h = 200, col = 'red')
+abline(v = 0.6,col = 'green')
+
+# par(mfrow = c(m,n))
+par(mfrow = c(1,1))
+plot(rate ~ conc,data = PuroA)
+
+# plot function 
+plot(rate ~conc,data = PuroA, pch = 2, col =4,
+     cex = 2.5 ,xlim =c(0,1.2), ylim = c(40,210),
+     ylab = "concentration",
+     xlab = "Rate",cex.main = 3     )
+title(main = "Puromycin", cex.lab = 1.2)
