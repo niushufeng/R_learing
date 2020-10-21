2a5dcfd37a1cca854f397fac5bddcb13cbb90ee2
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-21 20:00:36 +0800

    Create 学习通作业.R

---
+++ 2020_10_20/学习通作业.R
@@ -0,0 +1,25 @@
+#代码如下
+
+a = 3
+
+#写出输出结果
+
+a>5
+
+a<1
+
+names(mtcars)
+#col1 = mtcars$cyl
+attach(mtcars)
+cyl
+
+# 
+hist(mpg) # 频率直方图
+barplot(mpg) # 频数直方图
+boxplot(mpg) # 画框须图
+#
+
+a_mean <- c(1,2,3,4,9,11)
+mean(a_mean)  # 求平均值
+
+# 
