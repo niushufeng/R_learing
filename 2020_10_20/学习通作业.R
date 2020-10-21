bfdaa82b2a25ab36f418f9a8f946569ec6cba2dd
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-21 20:53:22 +0800

    Update 学习通作业.R

--- 2020_10_20/学习通作业.R
+++ 2020_10_20/学习通作业.R
@@ -23,3 +23,41 @@ a_mean <- c(1,2,3,4,9,11)
 mean(a_mean)  # 求平均值
 
 # 
+a_sd <-c(2,4,9,10,15)
+sd(a_sd)   #  标准差
+# 
+plot(cyl,mpg)
+
+# 第三章
+1/(52*5)
+
+# 
+1/prod(52:49)
+
+# 
+qnorm(0.01)
+qnorm(0.99)
+
+# 
+pbinom(4,25,0.2)
+qbinom(0.42,25,0.2)
+
+#
+?pbinom()
+?qbinom()
+
+# 
+rnorm(35)
+rnorm(35,mean = 2,sd = 5)
+
+# 
+sum(sample(1:100,5))
+
+#
+sd(sample(1:100,5))^2
+
+# 
+hist(sample(1:100,5))
+
+# 
+lines(density(x))
