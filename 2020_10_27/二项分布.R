dac3ac77ee8f61069606042ad6884dba5cf310be
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-27 15:31:41 +0800

    Update 二项分布.R

--- 2020_10_27/二项分布.R
+++ 2020_10_27/二项分布.R
@@ -4,3 +4,11 @@ p <-0.1
 dbinom(2,size,p)  ## 成功捉住两次的概率
 
 dbinom(0:size,size,p)  ## 整个概率分布
+
+
+n <- 20
+p <- 0.2
+k <- seq(0,n)
+plot(k,dbinom(k,n,p),type = 'h',
+      main = "Binomial distribution,
+     n = 20,p = 0.2",xlab = 'k')
