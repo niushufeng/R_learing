9d986113d716344a13e12e36217a70c17b4a822c
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-11-08 13:02:40 +0800

    Update 课后作业.R

--- 2020-11-03/课后作业.R
+++ 2020-11-03/课后作业.R
@@ -7,4 +7,18 @@ hist(x,xlim = c(min(x),max(x)),probability = T,nclass = max(x) - min(x) + 1,
      col = 'lightblue', main = 'Normal distribution, mean = 10, sd = 2') #绘制直方图
 lines(density(x,bw = 1),col = 'red',lwd = 3) # 添加核密度曲线
 
-#4.4
+#4.4由程序包DAAG中的数据集possum，
+#1)利用函数hist(possum$age)作出负鼠年龄的直方图，试图用这两种不同的断点并作比较，说明两图的不同之处；
+#2)求出负鼠年龄变量的均值、标准差、中位数以及上下四分位数
+install.packages("DAAG")
+library(DAAG)
+data(possum)
+par(mfrow=c(2,2))
+hist(possum$age,breaks=1+(0:8)*1)
+hist(possum$age,breaks=0+(0:9)*1)
+hist(possum$age,breaks=1+(0:5)*2)
+hist(possum$age,breaks=0+(0:5)*2)
+summary(possum$age)
+age<-possum$age[!is.na(possum$age)]
+summary(age)
+sd(age)
